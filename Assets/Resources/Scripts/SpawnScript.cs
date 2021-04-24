419d6cb4168348525b2e143c23099d1f9c3a4565
JotneDamnit <jornhenninglarsen@gmail.com>
2021-04-24 22:55:21 +0200

    Unity is not a nightmare at all

--- Assets/Resources/Scripts/SpawnScript.cs
+++ Assets/Resources/Scripts/SpawnScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SpawnScript : MonoBehaviour
 {
+    int fuckery = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class SpawnScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        fuckery = fuckery >= 5 ? 0 : fuckery++;
+
+        if (fuckery == 0)
+        {
+            GameObject cunt = Resources.Load("Prefabs/Enemy") as GameObject;
+            
+        }
     }
 }
