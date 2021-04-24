53def06613ded85ee242ab9d645515fcd8622882
thooom <tomerihov@gmail.com>
2021-04-24 22:43:39 +0200

    La til Turtle Enemy

--- Assets/Resources/Scripts/Turtle.cs
+++ Assets/Resources/Scripts/Turtle.cs
@@ -1,17 +1,30 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Turtle : MonoBehaviour
 {
-    float speed;
+    float speed, direction, upDown;
 
     void Start()
     {
+        direction = transform.position.x < 0 ? 1 : -1;
+        upDown = 0;
         speed = Random.Range(20, 35) / 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OutOfBounds();
+        transform.Translate(new Vector3(speed * direction * Time.deltaTime, upDown, 0));
+        upDown += Random.Range(-1f, 1f) / 1000; 
+    }
+
+    void OutOfBounds()
+    {
+        if (transform.position.x < -10 || transform.position.x > 10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
