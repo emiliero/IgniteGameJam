24ff20182f0bc7a6ee12f9af41b165ad25b7dd3a
JotneDamnit <jornhenninglarsen@gmail.com>
2021-04-25 23:04:16 +0200

    moar random

--- Assets/Resources/Scripts/SpawnScript.cs
+++ Assets/Resources/Scripts/SpawnScript.cs
@@ -5,15 +5,16 @@ using UnityEditor;
 
 public class SpawnScript : MonoBehaviour
 {
-    private int _tickCounter;
+    private long _tickCounter;
+    private int _snoppCountDown, _spawnNumber;
     private string[] _snopp = new string[3] { "Turtle", "Octopus", "TinyFish" };
-    private int[] _xOffsets = new int[2] { 5, -5 };
     private System.Random _chancifier = new System.Random();
 
     // Start is called before the first frame update
     void Start()
     {
         _tickCounter = 0;
+        _snoppCountDown = 0;
     }
 
     // Update is called once per frame
@@ -23,7 +24,7 @@ public class SpawnScript : MonoBehaviour
 
         if (_tickCounter % 120 == 0)
         {
-            int x = _xOffsets[_chancifier.Next(_xOffsets.Length)];
+            int x = _chancifier.Next(-10, 11);
             var snoppChooser = _chancifier.Next(_snopp.Length);
             var pos = new Vector3(x, GameObject.Find("Main Camera").transform.position.y - _chancifier.Next(3, 7), 0);
             var chosenSnopp = Resources.Load($"Prefabs/{_snopp[snoppChooser]}", typeof(GameObject)) as GameObject;
